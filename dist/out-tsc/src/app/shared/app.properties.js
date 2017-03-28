d54a7cdf97da7b59bf3eaef6adf0fffc60ffc0b9
vkraietskyi <vkraietskyi@softjourn.com>
2017-03-28 12:14:14 +0300

    Links update

--- dist/out-tsc/src/app/shared/app.properties.js
+++ dist/out-tsc/src/app/shared/app.properties.js
@@ -1,14 +1,14 @@
 export var AppProperties = (function () {
     function AppProperties() {
     }
-    AppProperties.API_COINS_ENDPOINT = 'https://sjcoins-testing.softjourn.if.ua/coins/api/v1';
+    AppProperties.API_COINS_ENDPOINT = 'https://sjcoins-testing.softjourn.if.ua/coins/v1';
     // static API_COINS_ENDPOINT = 'http://localhost:8080/api/v1';
     AppProperties.AUTH_SERVER = 'https://sjcoins-testing.softjourn.if.ua/auth';
     AppProperties.AUTH_ENDPOINT = AppProperties.AUTH_SERVER + '/oauth/token';
     // static ERIS_COMPILER = 'http://kraytsman.eris.compiler/compile';
     // static ERIS_COMPILER = 'http://46.101.203.71/compile';
-    AppProperties.ERIS_COMPILER = 'https://sjcoins-testing.softjourn.if.ua/coins/api/v1/contracts/compile';
-    AppProperties.AUTH_API = AppProperties.AUTH_SERVER + '/api/v1';
+    AppProperties.ERIS_COMPILER = 'https://sjcoins-testing.softjourn.if.ua/coins/v1/contracts/compile';
+    AppProperties.AUTH_API = AppProperties.AUTH_SERVER + '/v1';
     AppProperties.CLIENT_AUTH_HASH = 'dXNlcl9jcmVkOnN1cGVyc2VjcmV0';
     AppProperties.NOTIFICATION_OPTIONS = {
         position: ["bottom", "right"],
--- src/app/shared/app.properties.ts
+++ src/app/shared/app.properties.ts
@@ -1,12 +1,12 @@
 export class AppProperties {
-  static API_COINS_ENDPOINT = 'https://sjcoins-testing.softjourn.if.ua/coins/api/v1';
+  static API_COINS_ENDPOINT = 'https://sjcoins-testing.softjourn.if.ua/coins/v1';
   // static API_COINS_ENDPOINT = 'http://localhost:8080/api/v1';
   static AUTH_SERVER = 'https://sjcoins-testing.softjourn.if.ua/auth';
   static AUTH_ENDPOINT = AppProperties.AUTH_SERVER + '/oauth/token';
   // static ERIS_COMPILER = 'http://kraytsman.eris.compiler/compile';
   // static ERIS_COMPILER = 'http://46.101.203.71/compile';
-  static ERIS_COMPILER = 'https://sjcoins-testing.softjourn.if.ua/coins/api/v1/contracts/compile';
-  static AUTH_API = AppProperties.AUTH_SERVER + '/api/v1';
+  static ERIS_COMPILER = 'https://sjcoins-testing.softjourn.if.ua/coins/v1/contracts/compile';
+  static AUTH_API = AppProperties.AUTH_SERVER + '/v1';
   static CLIENT_AUTH_HASH = 'dXNlcl9jcmVkOnN1cGVyc2VjcmV0';
   static NOTIFICATION_OPTIONS = {
     position: ["bottom", "right"],
